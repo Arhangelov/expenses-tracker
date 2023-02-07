24cbd715ac0b73c82ddb9df9d1e1e253339d7118
Arhangelov <arhangelov93@gmail.com>
2023-02-07 03:33:44 +0200

    vercel configurations

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 const config = require("./config/main");
-const express = require('express');
+const express = require("express");
 const db = require("./config/mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const router = require('./routes');
+const router = require("./routes");
 
 const app = express();
 
-app.use(cors({credentials: true}));
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
 db().then(() => {
-    console.log('Connected to DB');
-    app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`));
+    console.log("Connected to DB");
+    app.listen(config.PORT, () =>
+        console.log(`Server is running on port ${config.PORT}`)
+    );
 });
